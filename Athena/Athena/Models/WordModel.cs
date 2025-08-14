d636c40aa832f88a21c9c3868b64f02e47b2efe9
Kornidesz Máté <mko@rushfiles.com>
2025-08-14 11:44:58 +0200

    example file added, reading of file solved

---
+++ Athena/Athena/Models/WordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Athena.Models
+{
+    public class WordModel
+    {
+        public string? GreekInGreek { get; set; }
+        public string? GreekInLatin { get; set; }
+        public string? EnglishTranslation { get; set; }
+    }
+}
--- Athena/Athena/ViewModels/ReadWordsViewModel.cs
+++ Athena/Athena/ViewModels/ReadWordsViewModel.cs
@@ -1,10 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Athena.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Athena.ViewModels
 {
@@ -53,14 +56,52 @@ namespace Athena.ViewModels
         }
 
         public IAsyncRelayCommand NavigateToMainMenu { get; private set; }
+        public IAsyncRelayCommand StartSession { get; private set; }
+
+        private readonly IDispatcherTimer _timer;
+        private int counter = 0;
+        private List<WordModel> _words = [];
 
         public ReadWordsViewModel()
         {
-            GreekInGreek = "Hello";
-            GreekInLatin = "Bello";
-            EnglishTranslation = "Mello";
-
             NavigateToMainMenu = new AsyncRelayCommand(GoToMainMenu);
+            StartSession = new AsyncRelayCommand(StartReading);
+
+            _timer = Dispatcher.GetForCurrentThread()!.CreateTimer();
+            _timer.Interval = TimeSpan.FromSeconds(5);
+            _timer.Tick += (s, e) =>
+            {
+                if(counter >= 150)
+                {
+                    _timer.Stop();
+                    return;
+                }
+
+                GreekInGreek = _words![counter].GreekInGreek;
+                GreekInLatin = _words![counter].GreekInLatin;
+                EnglishTranslation = _words![counter].EnglishTranslation;
+
+                counter++;
+            };
+        }
+
+        private async Task StartReading()
+        {
+            if (_words.Count > 0)
+                return;
+
+            using var stream = await FileSystem.OpenAppPackageFileAsync("example.json");
+            using var reader = new StreamReader(stream);
+            var json = await reader.ReadToEndAsync();
+
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            _words = JsonSerializer.Deserialize<List<WordModel>>(json, options)!;
+
+            _timer.Start();
         }
 
         private async Task GoToMainMenu()
